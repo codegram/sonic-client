710644e127885dd7a8013bb73361dc08c47d7916
Edgar Latorre <me@edgarlatorre.com.br>
2020-11-24 16:08:41 +0100

    Add ci workflow running sonic with docker compose

--- test/tcp_connection_test.exs
+++ test/tcp_connection_test.exs
@@ -4,7 +4,7 @@ defmodule ElixirSonicClient.TcpConnectionTest do
 
   describe "#start_link" do
     test "Successful connection" do
-      assert {:ok, conn} = TcpConnection.start_link(Kernel.to_charlist("sonic"), 1491, [])
+      assert {:ok, conn} = TcpConnection.start_link(host(), 1491, [])
       assert {:ok, message} = TcpConnection.recv(conn)
       assert "CONNECTED" <> _ = message
       TcpConnection.close(conn)
@@ -13,13 +13,13 @@ defmodule ElixirSonicClient.TcpConnectionTest do
 
   describe "#send" do
     test "send start search message" do
-      {:ok, conn} = TcpConnection.start_link(Kernel.to_charlist("sonic"), 1491, [])
+      {:ok, conn} = TcpConnection.start_link(host(), 1491, [])
       assert :ok = TcpConnection.send(conn, "START search SecretPassword")
       TcpConnection.close(conn)
     end
 
     test "send invalid mode" do
-      {:ok, conn} = TcpConnection.start_link(Kernel.to_charlist("sonic"), 1491, [])
+      {:ok, conn} = TcpConnection.start_link(host(), 1491, [])
       TcpConnection.recv(conn)
       assert :ok = TcpConnection.send(conn, "START invalid SecretPassword")
       assert {:ok, "ENDED invalid_mode"} = TcpConnection.recv(conn)
@@ -35,11 +35,15 @@ defmodule ElixirSonicClient.TcpConnectionTest do
   end
 
   defp connection(mode \\ "search") do
-    {:ok, conn} = TcpConnection.start_link(Kernel.to_charlist("sonic"), 1491, [])
+    {:ok, conn} = TcpConnection.start_link(host(), 1491, [])
     {:ok, _msg} = TcpConnection.recv(conn)
     :ok = TcpConnection.send(conn, "START #{mode} SecretPassword")
     {:ok, _msg} = TcpConnection.recv(conn)
 
     conn
+  end
+
+  defp host do
+    Kernel.to_charlist(System.get_env("SONIC_HOST", "sonic"))
   end
 end
