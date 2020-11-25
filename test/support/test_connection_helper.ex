e7beed60890994ee9c05a8257eaafb051bdbdcfe
Edgar Latorre <me@edgarlatorre.com.br>
2020-11-25 14:41:02 +0100

    Add connection helper for tests

---
+++ test/support/test_connection_helper.ex
@@ -0,0 +1,36 @@
+defmodule SonicClient.TestConnectionHelper do
+  alias SonicClient
+  @test_collection "test_collection"
+
+  def start_connection(mode) do
+    {:ok, conn} =
+      SonicClient.start(
+        host(),
+        1491,
+        mode,
+        "SecretPassword"
+      )
+
+    conn
+  end
+
+  def add_data(object, term, collection \\ @test_collection) do
+    conn = start_connection("ingest")
+    SonicClient.push(conn, collection, object, term)
+    stop_connection(conn)
+  end
+
+  def flush(collection \\ @test_collection) do
+    conn = start_connection("ingest")
+    SonicClient.flush(conn, collection)
+    stop_connection(conn)
+  end
+
+  def stop_connection(conn) do
+    SonicClient.stop(conn)
+  end
+
+  defp host do
+    Kernel.to_charlist(System.get_env("SONIC_HOST", "sonic"))
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
 ExUnit.start()
+Code.require_file("test_connection_helper.ex", "test/support/")
