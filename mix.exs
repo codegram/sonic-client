561281f333184e0ecf63403b9d542de183e78ad1
Edgar Latorre <me@edgarlatorre.com.br>
2020-11-26 16:02:41 +0100

    Add description, version and necessary links in the project

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule SonicClient.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :sonic_client,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -27,5 +32,27 @@ defmodule SonicClient.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:connection, "~> 1.1.0"}
     ]
+  end
+
+  defp description do
+    """
+    Client for Sonic which is a schema-less search backend.
+    As it still in beta it shouldn't be used in production yet.
+    """
+  end
+
+  defp docs do
+    [
+      source_url: "https://codegram.github.io/sonic-client/",
+      source_ref: "v#{@version}",
+      main: SonicClient
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/codegram/sonic-client"}
+    }
   end
 end
