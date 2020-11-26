14939a5f8b9122a6dd600e2c0161bd5abfc8670a
Edgar Latorre <me@edgarlatorre.com.br>
2020-11-26 12:36:58 +0100

    add tests for offset

--- test/modes/search_test.exs
+++ test/modes/search_test.exs
@@ -3,15 +3,15 @@ defmodule SonicClient.Modes.SearchTest do
   alias SonicClient.Modes.Search
   import SonicClient.TestConnectionHelper
 
+  setup_all do
+    add_data("user:1", "It is a common test")
+    add_data("user:2", "It is a common testable text")
+    add_data("user:3", "It should not appear in the common search")
+
+    on_exit(fn -> flush() end)
+  end
+
   describe "#query" do
-    setup do
-      add_data("user:1", "It is a test")
-      add_data("user:2", "It is a testable text")
-      add_data("user:3", "It should not appear in the search")
-
-      on_exit(fn -> flush() end)
-    end
-
     test "returns empty list" do
       conn = start_connection("search")
       assert {:ok, []} = Search.query(conn, "test_collection", "default_bucket", "non-existent")
@@ -35,11 +35,32 @@ defmodule SonicClient.Modes.SearchTest do
       stop_connection(conn)
     end
 
-    test "returns list of elements with limit 1" do
+    test "returns list of elements when limit 1" do
       conn = start_connection("search")
 
       assert {:ok, ["user:1"]} =
                Search.query(conn, "test_collection", "default_bucket", "test", limit: 1)
+
+      stop_connection(conn)
+    end
+
+    test "returns list of elements when offset 1" do
+      conn = start_connection("search")
+
+      assert {:ok, ["user:2"]} =
+               Search.query(conn, "test_collection", "default_bucket", "test", offset: 1)
+
+      stop_connection(conn)
+    end
+
+    test "returns list of elements when limit 1 and offset 1" do
+      conn = start_connection("search")
+
+      assert {:ok, ["user:2"]} =
+               Search.query(conn, "test_collection", "default_bucket", "common",
+                 limit: 1,
+                 offset: 1
+               )
 
       stop_connection(conn)
     end
